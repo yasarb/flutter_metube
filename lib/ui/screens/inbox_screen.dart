77f43cfa21e61031b8c38580741dad010240d1ee
yasar budulgan <yb.budulgan@gmail.com>
2019-05-20 23:44:25 +0300

    Update inbox screen. Add messages and notifications tabs

--- lib/ui/screens/inbox_screen.dart
+++ lib/ui/screens/inbox_screen.dart
@@ -3,8 +3,24 @@ import 'package:flutter/material.dart';
 class InboxScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('hello world'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: TabBar(
+          labelColor: Colors.black54,
+          indicatorColor: Colors.black87,
+          tabs: [
+            Tab(text: 'MESSAGES'),
+            Tab(text: 'NOTIFICATIONS'),
+          ],
+        ),
+        body: TabBarView(
+          children: [
+            Icon(Icons.message),
+            Icon(Icons.notifications),
+          ],
+        ),
+      ),
     );
   }
 }
